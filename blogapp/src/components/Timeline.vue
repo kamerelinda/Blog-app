f51afeee279fbad74c2b1c8399038195d21a9af3
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-12-05 11:49:49 +0300

    add first component

---
+++ blogapp/src/components/Timeline.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a href="">Today</a>
+      <a href="">Week</a>
+    </span>
+  </nav>
+</template>
