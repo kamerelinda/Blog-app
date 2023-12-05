9e488a18506075cdae6f9000b79306497d673c72
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-12-05 12:39:11 +0300

    use v-for for anchor tag

--- blogapp/src/components/Timeline.vue
+++ blogapp/src/components/Timeline.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const periods = ["Today", "This Week", "This Month"];
+
+
+const selectedPeriod = ref("Today");
+
+const selectPeriod = (period: string) => {
+    console.log(period),
+    selectedPeriod.value = period
+}
+</script>
 
 <template>
+    {{ selectedPeriod }}
   <nav class="is-primary panel">
     <span class="panel-tabs">
-      <a href="">Today</a>
-      <a href="">Week</a>
+      <a 
+        v-for="period in periods"
+        :key="period"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+    
+      </a>
     </span>
   </nav>
 </template>
